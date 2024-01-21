2156849b3eae192547a4a7ec576b1ed6aa908d3d
Nadirsah <isayevnadirsah26@gmail.com>
2024-01-22 00:42:33 +0400

    save_trans_change

---
+++ app/View/Composers/TranslateComposer.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\View\Composers;
+
+//use App\Repositories\UserRepository;
+use App\Models\Cart;
+use App\Models\Category;
+use App\Models\Lang;
+use App\Models\Product;
+use App\Models\TranslateHome;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+use Illuminate\View\View;
+class LangComposer
+{
+public function compose(View $view):void
+{   $user_id=Auth::id();
+    $cartItems =Cart::where('user_id',$user_id)->where('status', 0)->get();
+    $totalPrice = 0;
+    foreach ($cartItems as $cartItem) {
+        $product = Product::find($cartItem->product_id);
+        if ($product) {
+            $totalPrice += $product->price * $cartItem->quantity;
+        }
+    }
+
+    $transhome=TranslateHome::first();
+    $lang=Lang::get();
+    $categories=Category::with('children')->get();
+    $cart=Cart::where('user_id',$user_id )->where('status', 0)->sum('quantity');
+
+    $view->with('categories', $categories);
+    $view->with('lang', $lang);
+    $view->with('transhome', $transhome);
+    $view->with('cart', $cart);
+    $view->with('totalPrice', $totalPrice);
+}
+}
