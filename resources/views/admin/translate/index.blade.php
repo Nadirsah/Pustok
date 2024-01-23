debb4e677b3d11429a354633870326146aa48ae3
Nadirsah <isayevnadirsah26@gmaiL.com>
2024-01-23 17:18:25 +0400

    view_clean

--- resources/views/admin/translate/index.blade.php
+++ resources/views/admin/translate/index.blade.php
@@ -3,10 +3,8 @@
 <link href="{{asset('admin')}}\global_assets\css\icons\fontawesome\styles.min.css" rel="stylesheet" type="text/css">
 @endsection
 @section('theme_js')
-<script src="{{asset('admin')}}\global_assets\js\plugins\tables\datatables\datatables.min.js"></script>
+<script src="//cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
 <script src="{{asset('admin')}}\global_assets\js\plugins\forms\selects\select2.min.js"></script>
-<script src="{{asset('admin')}}\global_assets\js\demo_pages\datatables_advanced.js"></script>
-<script src="{{asset('admin')}}\global_assets\js\demo_pages\components_thumbnails.js"></script>
 
 @endsection
 
@@ -14,40 +12,39 @@
 @section('content')
 <div class="content">
     @include('layouts.admin.alert')
-    <div class="panel panel-flat">
-        <div class="table-responsive">
-            <div class="card-header header-elements-inline">
+    <div class="card">
+        <div class="card-header header-elements-inline">
+            <h5 class="card-title"><a href="{{route('translate.create')}}" class="btn btn-info"><i
+                        class="icon-plus3 mr-3 icon-xl"></i> Mehsul elave et</a></h5>
+        </div>
 
-                <h5 class="card-title"><a href="{{route('translate.create')}}" class="btn btn-info"><i
-                            class="icon-plus3 mr-3 icon-xl"></i> Mehsul elave et</a></h5>
-
-            </div>
-            <table class="table datatable-show-all">
+        <table class="table table-hover border table-bordered " id="dataTable"">
                 <thead>
                     <tr>
                         <th>Group</th>
                         <th>Key</th>
                         <th>Text</th>
-                        <th class="text-center">Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($data as $items)
-                    <tr>
-                        <td>{{$items->group}}</td>
-                        <td>{{$items->key}}</td>
-                        <td>{$items->text}</td>
-                        <td> <a href="{{route('translate.edit',$items->id)}}"><i
-                                    class="btn btn-info fa fa-edit"></i></a>
+                        <th class=" text-center">Actions</th>
+            </tr>
+            </thead>
+            <tbody>
+                @foreach($data as $items)
+                <tr>
+                    <td>{{$items->group}}</td>
+                    <td>{{$items->key}}</td>
+                    <td>{$items->text}</td>
+                    <td> <a href="{{route('translate.edit',$items->id)}}"><i class="btn btn-info fa fa-edit"></i></a>
 
-                        </td>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
     </div>
 </div>
 
+<script>
+let table = new DataTable('#dataTable');
+</script>
 
 @endsection
